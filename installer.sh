f8c3ec197bd9823f4b7f50210b80abf47f558b58
Luis Holanda <luiscmholanda@gmail.com>
2017-01-23 12:04:01 -0200

    Bring back case in installer.sh

--- installer.sh
+++ installer.sh
@@ -10,11 +10,13 @@ echo "Fetching latest version ..."
 latest=3.0.6-beta-5
 # latest=$(curl https://parsecli.back4app.com/supported?version=latest)
 
-url="https://github.com/back4app/parse-cli/releases/download/release_${latest}/b4a"
 
-if [ `uname` -eq "Linux" ]; then
-  url="${url}_linux"
-fi
+case `uname` in
+  "Linux" )
+      url="https://github.com/back4app/parse-cli/release/download/release_${latest}/b4a_linux"
+  "Darwin" )
+      url="https://github.com/back4app/parse-cli/release/download/release_${latest}/b4a"
+
 
 curl --progress-bar --compressed -Lo ${TMP_FILE} ${url}
 
@@ -26,3 +28,5 @@ fi
 echo "Installing ..."
 mv /tmp/back4app.tmp /usr/local/bin/b4a
 chmod 755 /usr/local/bin/b4a
+
+echo "Installation finished"
